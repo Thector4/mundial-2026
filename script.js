bf033b2bbe41ba34d0a4f76f94faa0bab7003e97
Thector4 <103524379+Thector4@users.noreply.github.com>
2026-06-19 10:41:58 +0200

    Centrar bien

--- script.js
+++ script.js
@@ -115,7 +115,7 @@ function renderGroups(data) {
         html += `
             <div class="group-card">
                 <div class="group-header">
-                    <span class="group-name"><center>${groupName}</center></span>
+                    <span class="group-name">${groupName}</span>
                 </div>
                 <table class="table">
                     <thead>
